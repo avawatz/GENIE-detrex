3513430d74c7ebd0cfcb513ca8599fd488d4bcd2
Srikeshram <110611485+srikesh-07@users.noreply.github.com>
2024-08-19 15:11:35 +0530

    Update coco_detr.py
    Modified the Config for the Manual Testing

--- configs/common/data/coco_detr.py
+++ configs/common/data/coco_detr.py
@@ -1,4 +1,5 @@
 from omegaconf import OmegaConf
+import os
 
 import detectron2.data.transforms as T
 from detectron2.config import LazyCall as L
@@ -8,14 +9,29 @@ from detectron2.data import (
     get_detection_dataset_dicts,
 )
 from detectron2.evaluation import COCOEvaluator
-
+from detrex.data.genie_dataset_mapper import GENIEDatasetDETRMapper
+from detrex.data.datasets.register_genie_dataset import register_genie_dataset
 from detrex.data import DetrDatasetMapper
 
 dataloader = OmegaConf.create()
 
+register_genie_dataset(name="devtest_Train",
+                       data_ids={"unlabelled_set": os.listdir("/content/kitti_dataset/images")[:25],
+                                 "augmented_set": os.listdir("/content/kitti_dataset/images")[25:50]},
+                       project_dir="/content/kitti_dataset",
+                       metadata={}
+                      )
+
+register_genie_dataset(name="devtest_Val",
+                       data_ids={"evaluation_set": os.listdir("/content/kitti_dataset/images")[50:75]},
+                       project_dir="/content/kitti_dataset",
+                       metadata={}
+                      )
+
 dataloader.train = L(build_detection_train_loader)(
-    dataset=L(get_detection_dataset_dicts)(names="coco_2017_train"),
-    mapper=L(DetrDatasetMapper)(
+    dataset=L(get_detection_dataset_dicts)(names="devtest_Train"),
+    mapper=L(GENIEDatasetDETRMapper)(
+        project_dir="/content/kitti_dataset",
         augmentation=[
             L(T.RandomFlip)(),
             L(T.ResizeShortestEdge)(
@@ -49,8 +65,9 @@ dataloader.train = L(build_detection_train_loader)(
 )
 
 dataloader.test = L(build_detection_test_loader)(
-    dataset=L(get_detection_dataset_dicts)(names="coco_2017_val", filter_empty=False),
-    mapper=L(DetrDatasetMapper)(
+    dataset=L(get_detection_dataset_dicts)(names="devtest_Val", filter_empty=False),
+    mapper=L(GENIEDatasetDETRMapper)(
+        project_dir="/content/kitti_dataset",
         augmentation=[
             L(T.ResizeShortestEdge)(
                 short_edge_length=800,
