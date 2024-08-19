1466a593e74e5205a558a32151e32f7ecd74685c
Srikesh <srikeshhp@gmail.com>
2024-08-19 09:15:32 +0530

    Initial repo commit and Added code for the DatasetMapper and Loading GENIE data in Detectron2 format.

---
+++ detrex/data/datasets/register_genie_dataset.py
@@ -0,0 +1,66 @@
+import os
+import json
+from detectron2.structures import BoxMode
+from detectron2.data import DatasetCatalog, MetadataCatalog
+
+
+def load_genie_dataset(data_ids, project_dir):
+    assert os.path.isdir(project_dir), "Project Directory doesn't exist."
+    if data_ids.get("augmented_set") is not None:
+        assert os.path.isdir(os.path.join(project_dir, "augmented_set")), "Augmented Set Directory doesn't exist."
+        data_ids["src_aug"] = os.listdir(os.path.join(project_dir, "augmented_set"))
+        assert data_ids["src_aug"] != 0, "Augmented Set is there but augmentation images in augmented_set folder is zero."
+    
+    all_anns = list()
+    ann_dir = os.path.join(project_dir, "annotations")
+    
+    for set_name, files in data_ids.items():
+        for name in files:
+            instance = dict()
+            instance["file_name"] = instance["image_id"] = name
+            instance["augmented"] = False if set_name != "src_aug" else True
+            with open(os.path.join(ann_dir, f"{os.path.splitext(name)[0]}.json")) as j_file:
+                data = json.load(j_file)
+            instance["height"], instance["width"] = data["size"]
+            instance["annotations"] = list()
+            if data.get("bboxes") is not None:
+                while len(data["bboxes"]):
+                    instance["annotations"].append(dict(bbox=data["bboxes"].pop(0),
+                                                        bbox_mode=BoxMode.XYXY_ABS,
+                                                        category_id=data["class_ids"].pop(0)))
+            elif data.get("segments") is not None:
+                while len(data["segments"]):
+                    if len(data["segments"][0]) >= 6 and len(data["segments"][0]) % 2 == 0:
+                        instance["annotations"].append(dict(bbox=data["bboxes"].pop(0),
+                                                            bbox_mode=BoxMode.XYXY_ABS,
+                                                            category_id=data["class_ids"].pop(0),
+                                                            segmentation=[data["segments"].pop(0)]))
+                    else:
+                        del data["segments"][0]
+                        del data["class_ids"][0]
+            else:
+                raise ValueError("Invalid JSON file passed.")
+            all_anns.append(instance)
+    
+    return all_anns
+
+
+            
+def register_genie_dataset(name: str,
+                           data_ids: dict,
+                           project_dir: str,
+                           metadata: dict):
+    
+    assert os.path.isdir(project_dir), "Project Directory doesn't exist."
+    if data_ids.get("augmented_set") is not None:
+        assert os.path.isdir(os.path.join(project_dir, "augmented_set")), "Augmented Set Directory doesn't exist."
+
+    DatasetCatalog.register(name, lambda: load_genie_dataset(data_ids=data_ids, 
+                                                             project_dir=project_dir))
+    
+    with open(os.path.join(project_dir, "metadata.json")) as j_file:
+        classes = json.load(j_file)
+
+    MetadataCatalog.get(name).set(
+        thing_classes=classes, evaluator_type="coco", **metadata
+    )
---
+++ detrex/data/genie_dataset_mapper.py
@@ -0,0 +1,69 @@
+import os
+import copy
+import logging
+import numpy as np
+import torch
+
+from detectron2.data import detection_utils as utils
+from detectron2.data import transforms as T
+
+
+class GENIEDatasetDETRMapper:
+    def __init__(self, project_dir: str, **kwargs):
+        self.project_dir = project_dir
+        super().__init__(**kwargs)
+
+    def __call__(self, dataset_dict):
+        """
+        Args:
+            dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
+
+        Returns:
+            dict: a format that builtin models in detectron2 accept
+        """
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        if dataset_dict["augmented"]:
+            img_path = os.path.join(self.project_dir, "augmented_set", dataset_dict["filesname"])
+        else:
+            img_path = os.path.join(self.project_dir, "images", dataset_dict["filesname"])
+        assert os.path.isfile(img_path), f"The Image {dataset_dict['filesname']} is not found."
+        image = utils.read_image(img_path, format=self.img_format)
+        utils.check_image_size(dataset_dict, image)
+
+        if self.augmentation_with_crop is None:
+            image, transforms = T.apply_transform_gens(self.augmentation, image)
+        else:
+            if np.random.rand() > 0.5:
+                image, transforms = T.apply_transform_gens(self.augmentation, image)
+            else:
+                image, transforms = T.apply_transform_gens(self.augmentation_with_crop, image)
+
+        image_shape = image.shape[:2]  # h, w
+
+        # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
+        # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
+        # Therefore it's important to use torch.Tensor.
+        dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose(2, 0, 1)))
+
+        if not self.is_train:
+            # USER: Modify this if you want to keep them for some reason.
+            dataset_dict.pop("annotations", None)
+            return dataset_dict
+
+        if "annotations" in dataset_dict:
+            # USER: Modify this if you want to keep them for some reason.
+            for anno in dataset_dict["annotations"]:
+                if not self.mask_on:
+                    anno.pop("segmentation", None)
+                anno.pop("keypoints", None)
+
+            # USER: Implement additional transformations if you have other types of data
+            annos = [
+                utils.transform_instance_annotations(obj, transforms, image_shape)
+                for obj in dataset_dict.pop("annotations")
+                if obj.get("iscrowd", 0) == 0
+            ]
+            instances = utils.annotations_to_instances(annos, image_shape)
+            dataset_dict["instances"] = utils.filter_empty_instances(instances)
+        return dataset_dict
+        
